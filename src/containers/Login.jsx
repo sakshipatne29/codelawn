cd380db00afc01ba5080eb7a9c8035ba50ccac89
Sakshi Patne <sakshipatne206@gmail.com>
2022-10-22 01:16:39 +0530

    Changes in login page

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,26 +1,58 @@
-import React from "react";
-import { useRef } from 'react';
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
+function LoginPage() {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <div className="container text-center mt-5 ">
+            <h1 className="" style={{ color: "greenyellow" }}>
+              CodeLawn
+            </h1>
+            <h1 className="border rounded-5 bg-white ">
+              Create. Build. Deploy.
+            </h1>
+          </div>
 
-const LoginPage = () => {
-    const form = useRef();
-    const sendEmail = (e) => {
-      e.preventDefault();
+          <div className="container text-center p-3 border rounded-5 bg-white mt-4">
+            <span className="border border-3 border-info rounded-5 p-2">
+              <a className="" href="www.google.com">
+                google
+              </a>
+            </span>
+            <hr class="mt-4" />
+            <Form className="p-4">
+              <Form.Group className="mb-3" controlId="formBasi Email">
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className="text-white">
+                  We'll never share your email with anyone else.
+                </Form.Text>
+              </Form.Group>
 
-    };
-    return (
-        <div className="container.login-container">
-            <h1 className="codelawn">CodeLawn</h1>
-            <h1 className="tagline">Create. Build. Deploy.</h1>
-            
-            <form ref={form} onSubmit={sendEmail}>
-                <a href="www.google.com" target="">Google</a>
-                <input type="email" name="email" placeholder="Email/Username" required />
-                <input type="password" name="password" placeholder="Password" required />
-                <button className="btn btn-primary" type="submit">Login</button>
-                <button className="btn btn-primary" type="submit">Forget Password</button>
-            </form>
-        </div>
-    )
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+              <Button className="ms-1 p-2 w-25" variant="primary" type="submit">
+                Login
+              </Button>
+              <Button className="ms-5 p-2" variant="primary" type="submit">
+                Forget Password
+              </Button>
+            </Form>
+          </div>
+        </Col>
+        <Col>
+          <div>
+            <img src="codelawn.jpg" alt="codelawn" />
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
+
 export default LoginPage;
