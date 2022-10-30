7d01e1ba55f120764bf982823ebed4b9e4234ede
Sakshi Patne <sakshipatne206@gmail.com>
2022-10-31 00:22:18 +0530

    icons and createApp

--- frontend/code/src/Components/ActionIcons.js
+++ frontend/code/src/Components/ActionIcons.js
@@ -1,12 +1,28 @@
 import { useState } from "react";
 import DeployIcon from "./AppIcons/DeployIcon";
+import EditIcon from "./AppIcons/EditIcon";
+import StartIcon from "./AppIcons/StartIcon";
+import StopIcon from "./AppIcons/StopIcon";
 
 function ActionIcons(props) {
 
   const [myIcon, setMyIcon] = useState("");
-  if(myIcon.length === 0) {
-    setMyIcon(<DeployIcon />)
-  }
+ if(myIcon.length === 0) {
+    console.log(props.iconName);
+    if(props.iconName === "deploy") {
+      setMyIcon(<DeployIcon />)
+    }
+    else if(props.iconName === "edit") {
+      setMyIcon(<EditIcon />) 
+    }
+    
+    else if(props.iconName === "start") {
+      setMyIcon(<StartIcon />)
+    }
+    else if(props.iconName === "stop"){
+      setMyIcon(<StopIcon />)
+    }
+ }
   return (
     <span >
       {myIcon}
--- frontend/code/src/Components/AppRecords.js
+++ frontend/code/src/Components/AppRecords.js
@@ -51,69 +51,17 @@ function AppRecords(props) {
                         Open App
                       </a>
                     ) : (
-                      <a>Open App</a>
+                      <a> Open App</a>
                     )}
                   </td>
                   <td className="text-end">
                     <ActionIcons iconName="deploy" />
-
-                    <span
-                      className="pe-4"
-                      data-bs-toggle="tooltip"
-                      data-bs-placement="bottom"
-                      title="Edit App"
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="25"
-                        height="25"
-                        fill="currentColor"
-                        className="bi bi-pencil-square"
-                        viewBox="0 0 16 16"
-                      >
-                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-                        <path
-                          fillRule="evenodd"
-                          d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"
-                        />
-                      </svg>
-                    </span>
+                    <ActionIcons iconName="edit" />
+                    
                     {record.status === "stopped" ? (
-                      <span
-                        className="pe-3"
-                        data-bs-toggle="tooltip"
-                        data-bs-placement="bottom"
-                        title="Start App"
-                      >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="35"
-                          height="35"
-                          fill="green"
-                          className="bi bi-play-fill"
-                          viewBox="0 0 16 16"
-                        >
-                          <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z" />
-                        </svg>
-                      </span>
+                      <ActionIcons iconName="start" />
                     ) : (
-                      <span
-                        className="pe-3"
-                        data-bs-toggle="tooltip"
-                        data-bs-placement="bottom"
-                        title="Stop App"
-                      >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="35"
-                          height="35"
-                          fill="red"
-                          className="bi bi-stop-fill"
-                          viewBox="0 0 16 16"
-                        >
-                          <path d="M5 3.5h6A1.5 1.5 0 0 1 12.5 5v6a1.5 1.5 0 0 1-1.5 1.5H5A1.5 1.5 0 0 1 3.5 11V5A1.5 1.5 0 0 1 5 3.5z" />
-                        </svg>
-                      </span>
+                      <ActionIcons iconName="stop" />
                     )}
                   </td>
                 </tr>
